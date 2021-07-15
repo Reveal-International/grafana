ca435f5a33b4d938e40d0e42f1d7b413fb8c5346
Steve Postill <steve.postill@revealinternational.com>
2021-07-15 14:43:16 +1200

    Delete old backup files.  Reverted test changes as causing conflicts when syncing with upstream.  Now we specifically ignore the tests

--- backup-sqlite3.sh
+++ backup-sqlite3.sh
@@ -9,3 +9,6 @@ ZIP=/bin/gzip
 mkdir -p ${BACKUP_FOLDER}
 ${SQLITE} ${DB} ".backup ${BACKUP}"
 ${ZIP} ${BACKUP}
+
+#now delete files older than 30 days (DB file is ~30K so not using up a lot of space)
+find /backup/grafana/grafana.db-*.bck.* -mtime +30 -exec rm {} \;
