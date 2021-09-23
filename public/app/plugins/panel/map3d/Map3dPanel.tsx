9d19332c80a0f9eaf977bc9325f76bf7c17426b5
Steve Sutton <steve.sutton@revealeinternational.com>
2021-09-24 09:53:24 +1200

    Start the extrusion

--- public/app/plugins/panel/map3d/Map3dPanel.tsx
+++ public/app/plugins/panel/map3d/Map3dPanel.tsx
@@ -1,7 +1,8 @@
 import { PanelProps } from '@grafana/data';
 import React from 'react';
+import * as turf from '@turf/turf';
 import { Map3dPanelOptions } from './types';
-import { Map, MapLayer, MapMarker } from '@grafana/ui/src/components/MapLibre';
+import { Map, MapLayer, MapMarker, MapSource } from '@grafana/ui/src/components/MapLibre';
 import { FillExtrusionPaint, LngLatLike } from 'maplibre-gl';
 
 const accessToken = 'get_your_own_OpIi9ZULNHzrESv6T2vL';
@@ -19,6 +20,31 @@ const extrusionPaint = {
 export function Map3dPanel(props: PanelProps<Map3dPanelOptions>) {
   // TODO need to solve how we update the map when props change
   const key = `${props.options.zoom}-${props.options.pitch}-${props.options.bearing}`;
+  const circle1 = turf.circle([174.77123737335205, -36.84480417706003], 0.01, {
+    properties: { height: 200, base_height: 0, color: 'orange' },
+  });
+  const circle2 = turf.circle([174.76419925689697, -36.84820411284973], 0.02, {
+    properties: { height: 500, base_height: 0, color: 'blue' },
+  });
+  const circle3 = turf.circle([174.7786783256836, -36.84820411284973], 0.02, {
+    properties: { height: 300, base_height: 0, color: 'green' },
+  });
+  const overlay = {
+    type: 'FeatureCollection',
+    features: [circle1, circle2, circle3],
+  };
+  const overlayPaint = {
+    // See the Mapbox Style Specification for details on data expressions.
+    // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions
+    // Get the fill-extrusion-color from the source 'color' property.
+    'fill-extrusion-color': ['get', 'color'],
+    // Get fill-extrusion-height from the source 'height' property.
+    'fill-extrusion-height': ['get', 'height'],
+    // Get fill-extrusion-base from the source 'base_height' property.
+    'fill-extrusion-base': ['get', 'base_height'],
+    // Make extrusions slightly opaque for see through indoor walls.
+    'fill-extrusion-opacity': 0.5,
+  };
 
   return (
     <Map
@@ -39,24 +65,14 @@ export function Map3dPanel(props: PanelProps<Map3dPanelOptions>) {
     >
       {/* render a standard marker marker */}
       <MapMarker lngLat={centerCoordinates} />
-      {/*<MapMarker lngLat={cnTower}>*/}
-      {/*  /!* render an html marker *!/*/}
-      {/*  <div>CN Tower</div>*/}
-      {/*</MapMarker>*/}
 
-      {/* render a layer */}
-
-      {/*<MapLayer*/}
-      {/*  id="area-fill"*/}
-      {/*  source="area"*/}
-      {/*  type="fill"*/}
-      {/*  paint={{*/}
-      {/*    'fill-color': 'red',*/}
-      {/*    'fill-opacity': 0.5,*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*  <MapSource id="area" type="geojson" data={exampleGeoJson.features[0] as GeoJSON.Feature} generateId />*/}
-      {/*</MapLayer>*/}
+      <MapSource type="geojson" id="overlay-source" data={overlay as GeoJSON.FeatureCollection} />
+      <MapLayer
+        id="overlay-extrusion"
+        type="fill-extrusion"
+        source="overlay-source"
+        paint={overlayPaint as FillExtrusionPaint}
+      ></MapLayer>
 
       <MapLayer
         id="3d-buildings"
