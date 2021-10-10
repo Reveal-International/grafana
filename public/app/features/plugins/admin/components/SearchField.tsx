ca879409c34a0cca8270b5a652db812c0860418a
Steve Postill <steve.postill@revealinternational.com>
2021-10-11 11:50:16 +1300

    merge of v8.2.0 of Grafana

--- public/app/features/plugins/admin/components/SearchField.tsx
+++ public/app/features/plugins/admin/components/SearchField.tsx
@@ -1,4 +1,3 @@
-import { FilterInput } from '@grafana/ui';
 import React, { useState, useRef } from 'react';
 import { useDebounce } from 'react-use';
 import { FilterInput } from 'app/core/components/FilterInput/FilterInput';
